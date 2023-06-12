df65ed69a5e551ad4adc0416ab8dee68aeebf659
Ioannis Nikolaidis <inikolaidis@novibet.com>
2023-06-12 15:14:13 +0300

    Added Swagger and Authorazation

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,5 +1,11 @@
+using BlueBrown.BigBola.Api;
+
 var webApplicationBuilder = WebApplication.CreateBuilder(args);
 
+webApplicationBuilder.Services.Configure();
+
 var webApplication = webApplicationBuilder.Build();
+
+webApplication.Configure();
 
 await webApplication.RunAsync();
